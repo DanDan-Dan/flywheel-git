f65231a96e1d65b00c63cb369f36018ce0b6d9f2
DanDan-Dan <148914957+DanDan-Dan@users.noreply.github.com>
2024-12-16 18:00:53 -0500

    Prime_Shoot

--- Flywheel.2/src/main/java/frc/robot/commands/Prime_Shoot.java
+++ Flywheel.2/src/main/java/frc/robot/commands/Prime_Shoot.java
@@ -4,16 +4,26 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Flywheel;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Prime_Shoot extends SequentialCommandGroup {
   /** Creates a new Prime_Shoot. */
-  public Prime_Shoot() {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+  public Prime_Shoot(Flywheel flywheel, Conveyor Convayer) {
+   
+    addCommands(  new InstantCommand(()-> flywheel.RunFullspeed(), flywheel),
+    new WaitUntilCommand(()-> flywheel.isFlywheelAtSetpoint()),
+    new InstantCommand(()-> Convayer.MoveNoteToShooter()),
+    new WaitCommand(2),
+    new InstantCommand(()-> flywheel.Stop())
+
+    );
   }
 }
